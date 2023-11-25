059b75526527bf4b3fa4f2d1ff2e2a654d25d4b4
Jose Villa <jose.villaprz@gmail.com>
2023-11-25 15:25:23 -0700

    Updates to results page WIP

--- app/attractions/page.tsx
+++ app/attractions/page.tsx
@@ -1,22 +1,35 @@
 import fetchAttractions from "@/app/util/fetchAttractions";
 
-export default async function Page({ searchParams }: { searchParams: { search: string } }) {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { search: string };
+}) {
   try {
     const { success, data } = await fetchAttractions(searchParams.search);
 
-    if (!success) {
-      return null;
-    }
+    // console.log(data);
 
     return (
-      <main className="container">
-        <section>
+      <main className="min-h-screen p-4 md:p-10 mx-auto max-w-7xl">
+        <section className="">
           <h1>Search results for "{searchParams.search}"</h1>
-          <div>
-            {data.length > 0
-              ? data.map((item) => <a href={`/attraction?id=${item.id}`}>{item.name}</a>)
-              : "No results"}
-          </div>
+          {data.length > 0 ? (
+            <ul>
+              {data.map((item) => (
+                <li key={item.id}>
+                  <a href={`/attraction?id=${item.id}`}>
+                    <article>
+                      <h2>{item.name}</h2>
+                      <p>Events: {item.eventsCount}</p>
+                    </article>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            "No Results"
+          )}
         </section>
       </main>
     );
